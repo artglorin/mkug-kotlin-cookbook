f337a572d7d1e74a84d20b886d6bd911c154cff6
v.verminskiy <develop@artglorin.com>
2019-02-28 10:38:45 +0300

    SampleNative file has been added

---
+++ src/sampleMain/kotlin/sample/SampleNative.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+    val arguments = args.toArguments()
+    for (index in arguments.count downTo 0) {
+        println("$index...")
+    }
+    println(arguments)
+}
+
+private fun Array<String>.toArguments() =
+    Arguments(
+        getOrNull(0).ifNullOrBlank { "Kotlin User Group" },
+        getOrNull(1).ifNullOrBlank { "Hello, {{name}}!" },
+        getOrNull(2)?.toIntOrNull() ?: 5
+
+    )
+
+private inline fun <C : CharSequence?, R : CharSequence> C.ifNullOrBlank(defaultValue: () -> R): R {
+    @Suppress("UNCHECKED_CAST")
+    return if (this.isNullOrBlank()) defaultValue() else this as R
+}
+
+data class Arguments(
+    val name: String,
+    val greetPattern: String,
+    val count: Int
+) {
+    override fun toString(): String {
+        return greetPattern.replace("{{name}}", name)
+    }
+}
